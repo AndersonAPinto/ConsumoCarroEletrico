b8854f1216f783227c852a1fc9166d46f63cc747
anderson <andersonaugustopinto@gmail.com>
2021-12-27 17:56:34 -0300

    primeiro commit

---
+++ scripts/script.js
@@ -0,0 +1,27 @@
+//Calculo da média de energia gasta para uma autonomia de determinado carro elértrico.
+//Se o seu elétrico mede a eficiência em quilômetros por kWh, basta calcular quanto custa cada km, dividindo o preço da eletricidade em kWh da sua região pelo número de quilômetros percorridos.
+let inputPot_bat=document.getElementById("pot_bat");
+let inputAutonomia=document.getElementById("autonomia");
+let inputKWh=document.getElementById("KWh");
+let inputRecarga=document.getElementById("recarga");
+
+let resultado=document.getElementById("resultado");
+
+function calcular(){
+    console.log("calculando...")
+
+    let Potenciabat= inputPot_bat.value;
+    let Autonomia= inputAutonomia.value;
+    let KWh= inputKWh.value;
+    
+    let Recarga=(Potenciabat * KWh)
+    let Custo= Recarga/Autonomia;
+    let qdtKmporkWh= Custo.toFixed(2);
+    console.log(qdtKmporkWh);
+    
+resultado.innerHTML=`<p>O resultado é de R$ ${qdtKmporkWh} por quilômetro.</p>`
+resultado.innerHTML +=`<p>E o custo para recarga desta Bateria de ${Potenciabat} KWh, é de R$ ${Recarga} Reais</p>`
+
+
+resultado.innerHTML +=`<p>OBS: A autonomia máxima informada é alcançada em condições específicas de condução do veículo, utilizando-se diferentes métodos de testes, em dinamômetro e na rua. Diversos fatores podem influenciar nos valores alcançados, tais como (modo de condução, quantidade de passageiros, relevo, peso embarcado, umidade, condições atmosféricas e das vias, pressão dos pneus, etc).</p>`
+}
